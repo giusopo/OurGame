22f7633affcf7ce0e25558f2a519a7229601f845
MarcoMeg03 <168180151+MarcoMeg03@users.noreply.github.com>
2026-03-13 16:43:28 +0100

    player animato
    player stilizzato ed animato

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     public float speed = 5f;
+	private Animator anim;
 
     private CharacterController controller;
     private Vector2 moveInput;
@@ -12,12 +13,19 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         controller = GetComponent<CharacterController>();
+        anim = gameObject.GetComponentInChildren<Animator>();
     }
 
     void Update()
     {
         Vector3 move = new Vector3(moveInput.x, 0, moveInput.y);
         controller.Move(move * speed * Time.deltaTime);
+
+        if (Input.GetKey ("w") || Input.GetKey ("a") || Input.GetKey ("s") || Input.GetKey ("d")) {
+            anim.SetInteger ("AnimationPar", 1);
+        }  else {
+            anim.SetInteger ("AnimationPar", 0);
+        }
     }
 
     public void OnMove(InputValue value)
